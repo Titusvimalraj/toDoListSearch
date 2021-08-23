133e9ed19ae6d34439ed4314d200298822661e16
Titus Vimal Raj <titusvimalraj@gmail.com>
2021-08-24 05:26:51 +0530

    First commit

---
+++ components/TodoList/TodoListItem/TodoListItem.js
@@ -0,0 +1,31 @@
+import Chip from '@material-ui/core/Chip';
+import DoneIcon from '@material-ui/icons/Done';
+import Avatar from '@material-ui/core/Avatar';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import ListIcon from '@material-ui/icons/List';
+
+const TodoListItem = ({ title, completed, handleDelete, handleClick }) => {
+    return (
+        <>
+            { completed ?
+            <Chip
+                avatar={<Avatar><DoneIcon /></Avatar>}
+                label={title}
+                onClick={handleClick}
+                color="secondary"
+                onDelete={handleDelete}
+            />:
+            <Chip
+                avatar={<Avatar><ListIcon /></Avatar>}
+                label={title}
+                onClick={handleClick}
+                color="default"
+                onDelete={handleDelete}
+            /> 
+        }
+        <br/>
+        </>
+    )
+}
+
+export default TodoListItem;
---
+++ pages/api/todo/index.js
@@ -0,0 +1,42 @@
+import dbConnect from '../../../lib/dbConnect'
+import Todo from '../../../models/Todo'
+
+export default async function handler(req, res) {
+  const { method } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const { search } = req.query;
+        let Todos
+        if(search != ""){/* find all the data in our database */
+          Todos = await Todo.find( 
+            { $text : { $search : `${req.query.search}`}},
+            { score : {$meta : "textScore"}},
+            { lean: true }
+          ).sort({ score : { $meta : 'textScore'}}); 
+        }else{
+          Todos = await Todo.find({}); 
+        } 
+        res.status(200).json({ success: true, data: Todos })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'POST':
+      try {
+        const todo = await Todo.create(
+          req.body
+        ) /* create a new model in the database */
+        res.status(201).json({ success: true, data: todo })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
