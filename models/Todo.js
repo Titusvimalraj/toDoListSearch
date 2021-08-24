c8da3f4c306164b19318d42d2c698176fcb549e3
Titus Vimal Raj <titusvimalraj@gmail.com>
2021-08-24 15:53:30 +0530

    code formatting applied

--- models/Todo.js
+++ models/Todo.js
@@ -1,20 +1,19 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 /* TodoSchema will correspond to a collection in your MongoDB database. */
 const TodoSchema = new mongoose.Schema({
   title: {
     /* The title of this Todo */
     type: String,
-    text : true,
-    required: [true, 'Please provide a title for this Todo.'],
-    maxlength: [60, 'Title cannot be more than 60 characters'],
-    
+    text: true,
+    required: [true, "Please provide a title for this Todo."],
+    maxlength: [60, "Title cannot be more than 60 characters"],
   },
   completed: {
     /* The status of this Todo */
     type: Boolean,
-    default: false
+    default: false,
   },
-})
+});
 
-export default mongoose.models.Todo || mongoose.model('Todo', TodoSchema)
+export default mongoose.models.Todo || mongoose.model("Todo", TodoSchema);
--- pages/index.js
+++ pages/index.js
@@ -1,178 +1,195 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
 import Head from "next/head";
 import TodoList from "../components/TodoList/TodoList";
 import styles from "../styles/Home.module.css";
-import { Grid, Button } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import SaveIcon from '@material-ui/icons/Save';
-import { BehaviorSubject, of, merge } from 'rxjs';
-import { debounceTime, map, distinctUntilChanged, filter, switchMap, catchError } from 'rxjs/operators';
-
+import { Grid, Button } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import SaveIcon from "@material-ui/icons/Save";
+import { BehaviorSubject, of, merge } from "rxjs";
+import {
+  debounceTime,
+  map,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+  catchError,
+} from "rxjs/operators";
 
 export default function Home() {
-  const contentType = 'application/json';
+  const contentType = "application/json";
   const [newTodoTitle, setNewTodoTitle] = useState("");
   const [state, setState] = useState({
     data: [],
     loading: false,
-    errorMessage: '',
-    noResults: false
+    errorMessage: "",
+    noResults: false,
   });
 
   const handleDelete = (id) => {
-     fetch(`/api/todo/${id}`,{
-          method: 'DELETE'
-          }).then(response => {
-              if(response.ok) {
-                return response
-                  .json()
-                  .then(data => {
-                      const afterDelete = [...state.data].filter(todo => todo._id !== id);
-                      setState((prevState)=>({...prevState, data:afterDelete}));
-                  });
-              }
-            }).catch(err=>console.log(err));
-  }
+    fetch(`/api/todo/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((data) => {
+            const afterDelete = [...state.data].filter(
+              (todo) => todo._id !== id
+            );
+            setState((prevState) => ({ ...prevState, data: afterDelete }));
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleClick = (todo) => {
-    const {_id:id, title, completed} = todo;    
-     fetch(`/api/todo/${id}`,{
-          method: 'PUT',
-          headers: {
-          Accept: contentType,
-          'Content-Type': contentType,
-        },
-        body: JSON.stringify({title, completed: !completed})
-          }).then(response => {
-              if(response.ok) {
-                return response
-                  .json()
-                  .then(data => {
-                       const updatedStatusArray = [...state.data].map(todo => {
-                        if (todo._id == id) {
-                          todo.completed = !todo.completed;
-                          return todo;
-                        }
-                        return todo;
-                      });
-
-                      setState((prevState)=>({...prevState, data:updatedStatusArray}));
-                  });
+    const { _id: id, title, completed } = todo;
+    fetch(`/api/todo/${id}`, {
+      method: "PUT",
+      headers: {
+        Accept: contentType,
+        "Content-Type": contentType,
+      },
+      body: JSON.stringify({ title, completed: !completed }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((data) => {
+            const updatedStatusArray = [...state.data].map((todo) => {
+              if (todo._id == id) {
+                todo.completed = !todo.completed;
+                return todo;
               }
-            }).catch(err=>console.log(err));  
-  }
+              return todo;
+            });
 
+            setState((prevState) => ({
+              ...prevState,
+              data: updatedStatusArray,
+            }));
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-  const addTodo = () =>{
-
-      fetch(`/api/todo`,{
-          method: 'POST',
-          headers: {
-          Accept: contentType,
-          'Content-Type': contentType,
-        },
-        body: JSON.stringify({title: newTodoTitle})
-          }).then(response => {
-              if(response.ok) {
-                return response
-                  .json()
-                  .then(newTodo => {
-                      setState((prevState)=>({...prevState, data:[...prevState.data, newTodo.data]}));
-                      setNewTodoTitle('');
-                  });
-              }
-            }).catch(err=>console.log(err));  
-  }
+  const addTodo = () => {
+    fetch(`/api/todo`, {
+      method: "POST",
+      headers: {
+        Accept: contentType,
+        "Content-Type": contentType,
+      },
+      body: JSON.stringify({ title: newTodoTitle }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((newTodo) => {
+            setState((prevState) => ({
+              ...prevState,
+              data: [...prevState.data, newTodo.data],
+            }));
+            setNewTodoTitle("");
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   const loadInitialData = () => {
-     fetch(`/api/todo?search=` ).then(response => {
-              if(response.ok) {
-                return response
-                  .json()
-                  .then(data => ({...data, loading: false, noResults: data.length === 0}));
-              }
-              return response
-                .json()
-                .then(data => ({
-                  data: [],
-                  loading: false,
-                  errorMessage: data.title
-                }));
-            }).then(newState=>{
-              setState(s => Object.assign({}, s, newState));
-            }).catch(err=>console.log(err));
-  }
-
- useEffect(() => {
-  loadInitialData();
- }, [])
-
-
-   const [subject, setSubject] = useState(null);
+    fetch(`/api/todo?search=`)
+      .then((response) => {
+        if (response.ok) {
+          return response
+            .json()
+            .then((data) => ({
+              ...data,
+              loading: false,
+              noResults: data.length === 0,
+            }));
+        }
+        return response.json().then((data) => ({
+          data: [],
+          loading: false,
+          errorMessage: data.title,
+        }));
+      })
+      .then((newState) => {
+        setState((s) => Object.assign({}, s, newState));
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    if(subject === null) {
-      const sub = new BehaviorSubject('');
+    loadInitialData();
+  }, []);
+
+  const [subject, setSubject] = useState(null);
+
+  useEffect(() => {
+    if (subject === null) {
+      const sub = new BehaviorSubject("");
       setSubject(sub);
     } else {
-      const observable = subject.pipe(
-        map(s => s.trim()),
-        distinctUntilChanged(),
-        filter(s => s.length >= 3),
-        debounceTime(200),
-        switchMap(term =>
-          merge(
-            of({loading: true, errorMessage: '', noResults: false}),
-            fetch(`/api/todo?search=${term}` ).then(response => {
-              if(response.ok) {
-                return response
-                  .json()
-                  .then(data => ({...data, loading: false, noResults: data.length === 0}));
-              }
-              return response
-                .json()
-                .then(data => ({
+      const observable = subject
+        .pipe(
+          map((s) => s.trim()),
+          distinctUntilChanged(),
+          filter((s) => s.length >= 3),
+          debounceTime(200),
+          switchMap((term) =>
+            merge(
+              of({ loading: true, errorMessage: "", noResults: false }),
+              fetch(`/api/todo?search=${term}`).then((response) => {
+                if (response.ok) {
+                  return response
+                    .json()
+                    .then((data) => ({
+                      ...data,
+                      loading: false,
+                      noResults: data.length === 0,
+                    }));
+                }
+                return response.json().then((data) => ({
                   data: [],
                   loading: false,
-                  errorMessage: data.title
+                  errorMessage: data.title,
                 }));
-            })
-          )
-        ),
-        catchError(e => ({
-          loading: false,
-          errorMessage: 'An application error occured'
-        }))
-      ).subscribe( newState => {
-        setState(s => Object.assign({}, s, newState));
-      });
+              })
+            )
+          ),
+          catchError((e) => ({
+            loading: false,
+            errorMessage: "An application error occured",
+          }))
+        )
+        .subscribe((newState) => {
+          setState((s) => Object.assign({}, s, newState));
+        });
 
       return () => {
-        observable.unsubscribe()
+        observable.unsubscribe();
         subject.unsubscribe();
-      }
+      };
     }
   }, [subject]);
 
-  const handleSearch = e => {
+  const handleSearch = (e) => {
     const term = e.target.value || "";
-    if(subject && term!="") {
+    if (subject && term != "") {
       return subject.next(term);
-    }else{
+    } else {
       loadInitialData();
     }
   };
 
-  const handleAdd = e => {
+  const handleAdd = (e) => {
     setNewTodoTitle(e.target.value);
-  }
-
-
-  
+  };
 
   const searchInput = useRef(null);
-  const addInput  = useRef(null);
-  
+  const addInput = useRef(null);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -185,102 +202,135 @@ export default function Home() {
         <h1 className={styles.title}>Welcome to Todo List App with Search!</h1>
 
         <p className={styles.description}>Get started by adding few To do`s</p>
-        <Grid container direction="row"
-              justifyContent="center"
-              alignItems="center"
-              spacing={4}>
-        <Grid item xs={12}>
-        <Grid container >
-          <Grid item xs>
-            <Grid container 
-            direction="row"
-                justifyContent="center"
-                alignItems="center"
-                spacing={1}
-            >
-              
-               <Grid item xs>
-              <Grid container><TextField inputRef={searchInput} onChange={handleSearch} label="Search Todos" variant="outlined" /></Grid>
-              
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={4}
+        >
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid item xs>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item xs>
+                    <Grid container>
+                      <TextField
+                        inputRef={searchInput}
+                        onChange={handleSearch}
+                        label="Search Todos"
+                        variant="outlined"
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item xs>
+                    <Grid container>
+                      <TextField
+                        value={newTodoTitle}
+                        label="Add Todo"
+                        variant="outlined"
+                        onChange={handleAdd}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid item xs>
+                    <Grid container>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        startIcon={<SaveIcon />}
+                        disabled={!newTodoTitle}
+                        onClick={addTodo}
+                      >
+                        Save
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs>
-              <Grid container 
-              direction="row"
-                justifyContent="center"
-                alignItems="center"
-                spacing={1}
-              >
-              
+          <Grid item xs={12}>
+            <Grid container>
               <Grid item xs>
-              <Grid container><TextField value={newTodoTitle} label="Add Todo" variant="outlined" onChange={handleAdd}/></Grid>
-              
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={4}
+                >
+                  <h2>All</h2>
+                  <TodoList
+                    todos={state.data}
+                    handleDelete={handleDelete}
+                    handleClick={handleClick}
+                  />
+                </Grid>
               </Grid>
               <Grid item xs>
-              <Grid container>             
-               <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        startIcon={<SaveIcon />}
-        disabled={!newTodoTitle}
-        onClick={addTodo}
-      >
-        Save
-      </Button>
-      </Grid>
-
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={4}
+                >
+                  <h2>Pending</h2>
+                  <TodoList
+                    todos={
+                      state.data.length
+                        ? state.data.filter((todo) => todo.completed == false)
+                        : []
+                    }
+                    handleDelete={handleDelete}
+                    handleClick={handleClick}
+                  />
+                </Grid>
               </Grid>
-              
-
-      
+              <Grid item xs>
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={4}
+                >
+                  <h2>Completed</h2>
+                  <TodoList
+                    todos={
+                      state.data.length
+                        ? state.data.filter((todo) => todo.completed == true)
+                        : []
+                    }
+                    handleDelete={handleDelete}
+                    handleClick={handleClick}
+                  />
+                </Grid>
               </Grid>
-           </Grid>
-        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs>
-            
-          <Grid container direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={4}
-          >
-            <h2>All</h2>
-            <TodoList todos={state.data} handleDelete={handleDelete} handleClick={handleClick} />
+            </Grid>
           </Grid>
-          </Grid>
-          <Grid item xs>
-          <Grid container direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={4}
-          >
-            <h2>Pending</h2>
-            <TodoList todos={state.data.length ? state.data.filter(todo => todo.completed==false) : []} handleDelete={handleDelete} handleClick={handleClick} />
-          </Grid>
-          </Grid>
-          <Grid item xs>
-          <Grid container direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={4}
-          >
-            <h2>Completed</h2>
-            <TodoList todos={state.data.length ? state.data.filter(todo => todo.completed==true) : []} handleDelete={handleDelete} handleClick={handleClick} />
-          </Grid>
-          </Grid>
-          
-          </Grid>
-        </Grid>
         </Grid>
       </main>
 
-      <footer className={styles.footer}>
-        Powered by Titus Vimal Raj
-      </footer>
+      <footer className={styles.footer}>Powered by Titus Vimal Raj</footer>
     </div>
   );
 }
